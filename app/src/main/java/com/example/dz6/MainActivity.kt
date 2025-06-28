c772fcae023874dc4b486fe5128a4e0f609cda06
Tat-T <ring_87_87@mail.ru>
2025-06-28 20:27:10 +0300

    Update

--- app/src/main/java/com/example/dz6/MainActivity.kt
+++ app/src/main/java/com/example/dz6/MainActivity.kt
@@ -1,7 +1,15 @@
 package com.example.dz6
 
+import android.Manifest
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
@@ -34,6 +42,10 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
 
 import com.example.dz6.ui.theme.AppTheme
 
@@ -41,13 +53,33 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        createNitificationChannel()
         setContent {
             AppTheme {
                 AppScreenPreview()
                 }
             }
         }
+
+    private fun createNitificationChannel(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val CANNEL_ID = null
+            val channel = NotificationChannel(
+               CANNEL_ID,
+               "My Notifications",
+               NotificationManager.IMPORTANCE_DEFAULT
+           ).apply {  description = "Channel for app notofocations" }
+            val  notificationManager = getSystemService(NotificationManager::class.java)
+            notificationManager.createNotificationChannels(channel)
+        }
     }
+
+    companion object {
+        const val CHANNEL_ID = "my_app_channel"
+        const val NOTIFICATION_ID =1
+    }
+}
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -56,6 +88,13 @@ fun AppScreen(){
     var showEditDialog by remember { mutableStateOf(false) }
     var tempText by remember {mutableStateOf("")}
     var context = LocalContext.current
+    val hasNotificationPermission = remember {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            ContextCompat.checkSelfPermission(context,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) == PackageManager.PERMISSION_GRANTED
+        }
+    }
 
     Scaffold(
         topBar = {
@@ -120,6 +159,25 @@ fun AppScreen(){
                             },
             dismissButton = { TextButton(onClick = { showEditDialog = false }) { Text("CANCEL") }}
             )
+    }
+}
+
+fun showNotification(context: Context, message: String) {
+    val builder = NotificationCompat.Builder(context, MainActivity.CHANNEL_ID)
+        .setSmallIcon(android.R.drawable.ic_dialog_info)
+        .setContentTitle("From my app")
+        .setContentText(message)
+        .setPriority(Notification.PRIORITY_DEFAULT)
+
+    with(NotificationManagerCompat.from(context)) {
+        if (ActivityCompat.checkSelfPermission(
+            context,
+                Manifest.permission.POST_NOTIFICATIONS
+        ) != PackageManager.PERMISSION_GRANTED
+            ) {
+            return
+        }
+        notify(MainActivity.NOTIFICATION_ID, builder.build())
     }
 }
 
